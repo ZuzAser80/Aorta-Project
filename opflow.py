55eede7173a0f79c72514e0bdd284c9e12ef4375
ZuzAser80 <dde4th@gmail.com>
2024-04-27 22:29:15 +0500

    feat: added vectors, intend to use them for finding the pixels. TODO: update the pixel mask to not include circle at the top.

--- opflow.py
+++ opflow.py
@@ -1,8 +1,6 @@
-import math
+import VectorsPY as vec
 import cv2
 import numpy as np
-import os as os
-from matplotlib import pyplot as plt
 
 arr = []
 _ = None
@@ -21,8 +19,18 @@ def dist(point0, point1):
     return np.sqrt(((point1[0] - point0[0]) ** 2 + (point1[1] - point0[1]) ** 2))
 
 
+def compare_delta(points0, points1, l):
+    d00 = points0[0][0][0] - points0[1][0][0]
+    d01 = points0[0][0][1] - points0[1][0][1]
+    d10 = points1[0][0][0] - points1[1][0][0]
+    d11 = points1[0][0][1] - points1[1][0][1]
+    return abs(d00 - d10) <= l and abs(d01 - d11) <= l
+
+
 cap = cv2.VideoCapture('projectoid2.mp4')
 dic = {}
+vec_mag_dict = {}
+vec_dir_dict = {}
 size = (int(cap.get(cv2.CAP_PROP_FRAME_WIDTH)), int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT)))
 frameRate = cap.get(cv2.CAP_PROP_FPS)
 a, _ = cap.read()
@@ -32,7 +40,6 @@ def waitUserInput(_):
     print("----")
     cv2.namedWindow('image')
     cv2.setMouseCallback('image', input_point)
-    cntr = ()
 
     while (1):
         cv2.imshow('image', _)
@@ -42,16 +49,37 @@ def waitUserInput(_):
             break
         elif k == ord('a'):
             print(mouseX, mouseY)
-    for p in range(1, len(arr)):
-        dic[dist(arr[0], arr[p])] = arr[p]
+    cntr = ()
+    img_hsv = cv2.cvtColor(_, cv2.COLOR_BGR2HSV)
+    lower_blue = np.array([100, 50, 50])
+    upper_blue = np.array([130, 255, 255])
+    mask_red = cv2.inRange(img_hsv, lower_blue, upper_blue)
+    contours, _ = cv2.findContours(mask_red, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+    for contour in contours:
+        M = cv2.moments(contour)
+        if M["m00"] != 0:
+            cX = int(M["m10"] / M["m00"])
+            cY = int(M["m01"] / M["m00"])
+            cv2.circle(_, (cX, cY), 10, (255, 255, 0), -1)
+            cntr = (cX, cY)
+    print(":;:::", vec.Vector2(cntr[0] + 69, cntr[1] + 69).direction())
+    for p in range(len(arr)):
+        vector = vec.Vector2(abs(cntr[0] - arr[p][0]), abs(cntr[1] - arr[p][1]))
+        vec_mag_dict[vector.magnitude()] = vector.direction()
+        vec_dir_dict[vector.direction()] = vector.magnitude()
+        for i in range(-10, 10):
+            dic[dist(arr[0], arr[p]) + i] = arr[p]
+            dic[dist(cntr, arr[p]) + i] = arr[p]
 
 
 v = []
 last_point = ()
 written = False
+written1 = False
+
 
 def track():
-    global written, last_point
+    global written, last_point, written1
     f = open("opflow_res.txt", "w")
     f.write(("xCent -- yCent -- xP -- yP -- xV -- yV -- t" + "\n"))
     while True:
@@ -76,19 +104,19 @@ def track():
         nonzero = cv2.findNonZero(mask_red)
         f.write(str(current_center[0]) + " -- " + str(current_center[1]) + " -- ")
         for p in nonzero:
-            if dist(arr[0], p[0]) in dic:
-                f.write(str(p[0][0]) + " -- " + str(p[0][1]) + " -- ")
-                last_point = p[0]
-                cv2.circle(frame, p[0], 5, (0, 255, 255), -1)
-                written = True
+            vector = vec.Vector2(abs(current_center[0] - p[0][0]), abs(current_center[1] - p[0][1]))
+            print("curdir:", vector.direction())
+            if any(l - vector.direction() <= 0.1 for l in vec_dir_dict):
+                cv2.circle(frame, p[0], 5, (255, 0, 255), -1)
+                print("YIPEE")
                 break
-        if not written and len(last_point) > 0:
-            f.write(str(last_point[0]) + " -- " + str(last_point[1]) + " -- ")
-            written = True
         f.write(str(frameId) + "\n")
         cv2.imshow("frame", frame)
         v.append(frame)
         written = False
+        written1 = False
+        if frameId % 10 == 0:
+            cv2.imwrite("frame_" + str(frameId) + ".png", frame)
         k = cv2.waitKey(25) & 0xFF
         if k == 27:
             break
