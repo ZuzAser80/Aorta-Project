984f9a04e8abddee81ea18d9bd2697815f2478c2
ZuzAser80 <dde4th@gmail.com>
2024-04-30 22:24:31 +0500

    feat: app :: fix: allowed leeway

---
+++ app_test.py
@@ -0,0 +1,36 @@
+import cv2
+
+import tkinter as tk
+from tkinter import filedialog
+from PIL import Image, ImageTk
+
+import opflow
+
+
+def open_vid():
+    file_path = filedialog.askopenfilename(title="Выберите видео", filetypes=[("Video files", "*.mp4")])
+    if file_path:
+        display_image(file_path)
+
+
+def display_image(file_path):
+    global kill
+    status_label.config(text=f"Загруженный файл: {file_path}")
+    opflow.track_video(file_path, entry_field.get(), True)
+
+
+root = tk.Tk()
+root.title("AORTA VALVE TRACKING")
+text_widget = tk.Text(root, wrap=tk.WORD, height=15, width=35)
+open_button = tk.Button(root, text="Выбрать видео", command=open_vid)
+open_button.pack(padx=20, pady=10)
+entry_field = tk.Entry()
+entry_field.pack(anchor=tk.NW, padx=30, pady= 10)
+# enabled = tk.IntVar()
+# enabled_checkbutton = tk.Checkbutton(text="Выводить видео", variable=enabled)
+# enabled_checkbutton.pack(padx=40, pady=10, anchor=tk.NW)
+status_label = tk.Label(root, text="", padx=20, pady=10)
+status_label.pack()
+
+root.mainloop()
+
--- opflow.py
+++ opflow.py
@@ -8,151 +8,153 @@ arr = []
 _ = None
 
 
-#input
-def input_point(event, x, y, flags, param):
-    global mouseX, mouseY
-    if event == cv2.EVENT_LBUTTONDOWN:
-        cv2.circle(_, (x, y), 10, (255, 0, 0), -1)
-        arr.append((x, y))
-        mouseX, mouseY = x, y
-
-
 def dist(point0, point1):
     return np.sqrt(((point1[0] - point0[0]) ** 2 + (point1[1] - point0[1]) ** 2))
 
 
-cap = cv2.VideoCapture('projectoid2.mp4')
-vec_mag_dict = {}
-vec_dir_dict = {}
-flip = False
-flips = {}
-size = (int(cap.get(cv2.CAP_PROP_FRAME_WIDTH)), int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT)))
-frameRate = cap.get(cv2.CAP_PROP_FPS)
-a, _ = cap.read()
+def track_video(filename: str, output_name: str, show_video: bool) -> bool:
+    cap = cv2.VideoCapture(filename)
+    vec_dir_dict = {}
+    flips = {}
+    size = (int(cap.get(cv2.CAP_PROP_FRAME_WIDTH)), int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT)))
+    frameRate = cap.get(cv2.CAP_PROP_FPS)
+    a, _ = cap.read()
 
+    def input_point(event, x, y, flags, param):
+        global mouseX, mouseY
+        if event == cv2.EVENT_LBUTTONDOWN:
+            cv2.circle(_, (x, y), 10, (255, 0, 0), -1)
+            arr.append((x, y))
+            mouseX, mouseY = x, y
 
-def waitUserInput(_):
-    global mn, flip
-    print("----")
-    cv2.namedWindow('image')
-    cv2.setMouseCallback('image', input_point)
+    def waitUserInput(_):
+        print("----")
+        cv2.namedWindow("windowName")
+        cv2.setMouseCallback("windowName", input_point)
 
-    while (1):
-        cv2.imshow('image', _)
-        cv2.imwrite("circle_picker.png", _)
-        k = cv2.waitKey(20) & 0xFF
-        if k == 27:
-            break
-        elif k == ord('a'):
-            print(mouseX, mouseY)
-    cntr = []
-    img_hsv = cv2.cvtColor(_, cv2.COLOR_BGR2HSV)
-    lower_blue = np.array([110, 150, 150])
-    upper_blue = np.array([130, 255, 255])
-    mask_red = cv2.inRange(img_hsv, lower_blue, upper_blue)
-    contours, _ = cv2.findContours(mask_red, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
-    for contour in contours:
-        M = cv2.moments(contour)
-        if M["m00"] != 0:
-            cX = int(M["m10"] / M["m00"])
-            cY = int(M["m01"] / M["m00"])
-            cntr.append((cX, cY))
-            print("u:", cntr)
-            cv2.circle(_, (cX, cY), 10, (255, 255, 0), -1)
-            #break
-
-    for p in range(len(arr)):
-        i = 0
-        if len(cntr) > 1 and not cntr[1] == arr[p]:
-            i = 1
-        vector = vec.Vector2(cntr[i][0] - arr[p][0], (cntr[i][1] - arr[p][1]))
-        if vector.y > 0:
-            flips[p] = True
-        print(":", vector.magnitude(), " :: ", vector.direction(), ":", vector.y)
-        vec_dir_dict[vector.direction()] = vector.magnitude()
-        mn = vector.direction()
-
-
-v = []
-last_vector = vec.Vector2(0, 0)
-
-
-def track():
-    global last_vector
-    f = open("opflow_res.txt", "w")
-    f.write("xCent -- yCent -- ")
-    for el in range(len(arr)):
-        f.write("P#" + str(el) + "X -- P#" + str(el) + "Y -- ")
-    f.write("t" + "\n")
-    while True:
-        frameId = cap.get(1)
-        ret, frame = cap.read()
-        if not ret:
-            break
-        img_hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+        while 1:
+            cv2.imshow("windowName", _)
+            cv2.imwrite("circle_picker.png", _)
+            k = cv2.waitKey(20) & 0xFF
+            if k == 27:
+                break
+            elif k == ord('a'):
+                print(mouseX, mouseY)
+        cntr = []
+        img_hsv = cv2.cvtColor(_, cv2.COLOR_BGR2HSV)
         lower_blue = np.array([110, 150, 150])
         upper_blue = np.array([130, 255, 255])
         mask_red = cv2.inRange(img_hsv, lower_blue, upper_blue)
-        current_center = (0, 0)
         contours, _ = cv2.findContours(mask_red, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
         for contour in contours:
             M = cv2.moments(contour)
             if M["m00"] != 0:
                 cX = int(M["m10"] / M["m00"])
                 cY = int(M["m01"] / M["m00"])
-                cv2.circle(frame, (cX, cY), 10, (255, 255, 0), -1)
-                current_center = (cX, cY)
+                cntr.append((cX, cY))
+                cv2.circle(_, (cX, cY), 10, (255, 255, 0), -1)
+                #break
+
+        for p in range(len(arr)):
+            i = 0
+            if len(cntr) > 1 and not cntr[1] == arr[p]:
+                i = 1
+            vector = vec.Vector2(cntr[i][0] - arr[p][0], (cntr[i][1] - arr[p][1]))
+            if vector.y > 0:
+                flips[p] = True
+            print(":", vector.magnitude(), " :: ", vector.direction(), ":", vector.y)
+            vec_dir_dict[vector.direction()] = vector.magnitude()
+    v = []
+
+    def track():
+        f = open(output_name + ".txt", "w")
+        f.write("xCent -- yCent -- ")
+        for el in range(len(arr)):
+            f.write("P#" + str(el) + "X -- P#" + str(el) + "Y -- ")
+        f.write("t" + "\n")
+        while True:
+            frameId = cap.get(1)
+            ret, __ = cap.read()
+            if not ret:
                 break
-        nonzero = cv2.findNonZero(mask_red)
-        f.write(str(current_center[0]) + " -- " + str(current_center[1]) + " -- ")
-        for key, value in vec_dir_dict.items():
-            y = value * math.cos(math.radians(key))
-            x = value * math.sin(math.radians(key))
-            if list(vec_dir_dict.keys()).index(key) in list(flips.keys()) and flips[list(vec_dir_dict.keys()).index(key)]:
-                y = -y
-                x = -x
-            #1st vector from the middle(center)
-            vector = vec.Vector2(x, y)
-            proj = (current_center[0] + int(vector.x), current_center[1] + int(vector.y))
-            #display it
-            cv2.circle(frame, proj, 5, (0, 127, 0), -1)
-            #looking for closest to the original vector's end blue pixel
-            distances = np.sqrt((nonzero[:, :, 0] - proj[0]) ** 2 + (nonzero[:, :, 1] - proj[1]) ** 2)
-            nearest_index = np.argmin(distances)
-            #display it
-            cv2.circle(frame, nonzero[nearest_index][0], 5, (0, 127, 127), -1)
-            #2nd vector: from the middle through the closest blue pixel(to determine the angle of the thing)
-            x1 = abs(current_center[0] - nonzero[nearest_index][0][0])
-            y1 = abs(current_center[1] - nonzero[nearest_index][0][1])
-            vector1 = vec.Vector2(x1, y1).unitvector()
-            #setting the magnitude
-            vector1.x *= abs(value)
-            vector1.y *= abs(value)
-            #display it
-            proj1 = (current_center[0] + int(vector1.x * (x/abs(x))), current_center[1] + int(vector1.y * (y/abs(y))))
-            cv2.circle(frame, proj1, 5, (0, 0, 255), -1)
-            #looking for the closest to 2nd vector blue pixel(should be final (?))
-            distances = np.sqrt((nonzero[:, :, 0] - proj1[0]) ** 2 + (nonzero[:, :, 1] - proj1[1]) ** 2)
-            nearest_index_1 = np.argmin(distances)
-            # display it
-            cv2.circle(frame, nonzero[nearest_index_1][0], 5, (255, 255, 255), -1)
-            #write to file
-            f.write(str(nonzero[nearest_index_1][0][0]) + " -- " + str(nonzero[nearest_index_1][0][1]) + " -- ")
-        f.write(str(frameId) + "\n")
-        cv2.imshow("frame", frame)
-        v.append(frame)
-        if frameId % 10 == 0:
-            cv2.imwrite("frame_" + str(frameId) + ".png", frame)
-        k = cv2.waitKey(25) & 0xFF
-        if k == 27:
-            break
+            frame = __.copy()
+            img_hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+            lower_blue = np.array([110, 150, 150])
+            upper_blue = np.array([130, 255, 255])
+            mask_red = cv2.inRange(img_hsv, lower_blue, upper_blue)
+            current_center = (0, 0)
+            contours, _ = cv2.findContours(mask_red, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+            for contour in contours:
+                M = cv2.moments(contour)
+                if M["m00"] != 0:
+                    cX = int(M["m10"] / M["m00"])
+                    cY = int(M["m01"] / M["m00"])
+                    cv2.circle(frame, (cX, cY), 10, (255, 255, 0), -1)
+                    current_center = (cX, cY)
+                    break
+            nonzero = cv2.findNonZero(mask_red)
+            f.write(str(current_center[0]) + " -- " + str(current_center[1]) + " -- ")
+            for key, value in vec_dir_dict.items():
+                y = value * math.cos(math.radians(key))
+                x = value * math.sin(math.radians(key))
+                if list(vec_dir_dict.keys()).index(key) in list(flips.keys()) and flips[
+                    list(vec_dir_dict.keys()).index(key)]:
+                    y = -y
+                    x = -x
+                #1st vector from the middle(center)
+                vector = vec.Vector2(x, y)
+                proj = (current_center[0] + int(vector.x), current_center[1] + int(vector.y))
+                #display it
+                cv2.circle(frame, proj, 5, (0, 127, 0), -1)
+                #looking for closest to the original vector's end blue pixel
+                distances = np.sqrt((nonzero[:, :, 0] - proj[0]) ** 2 + (nonzero[:, :, 1] - proj[1]) ** 2)
+                nearest_index = np.argmin(distances)
+                #display it
+                cv2.circle(frame, nonzero[nearest_index][0], 5, (0, 127, 127), -1)
+                #2nd vector: from the middle through the closest blue pixel(to determine the angle of the thing)
+                x1 = abs(current_center[0] - nonzero[nearest_index][0][0])
+                y1 = abs(current_center[1] - nonzero[nearest_index][0][1])
+                vector1 = vec.Vector2(x1, y1).unitvector()
+                #setting the magnitude
+                vector1.x *= abs(value)
+                vector1.y *= abs(value)
+                #display it
+                proj1 = (
+                    current_center[0] + int(vector1.x * (x / abs(x))),
+                    current_center[1] + int(vector1.y * (y / abs(y))))
+                cv2.circle(frame, proj1, 5, (0, 0, 255), -1)
+                #looking for the closest to 2nd vector blue pixel(should be final (?))
+                distances = np.sqrt((nonzero[:, :, 0] - proj1[0]) ** 2 + (nonzero[:, :, 1] - proj1[1]) ** 2)
+                nearest_index_1 = np.argmin(distances)
+                # display it
+                true_point = nonzero[nearest_index_1][0]
+                if dist(nonzero[nearest_index_1][0], proj1) <= 10:
+                    true_point = proj1
+                cv2.circle(frame, true_point, 5, (255, 255, 255), -1)
+                #write to file
+                f.write(str(true_point[0]) + " -- " + str(true_point[1]) + " -- ")
+            f.write(str(frameId) + "\n")
+            if show_video:
+                cv2.imshow("frame", frame)
+            v.append(frame)
+            if frameId % 10 == 0:
+                cv2.imwrite("frame_" + str(frameId) + ".png", frame)
+            k = cv2.waitKey(25) & 0xFF
+            if k == 27:
+                break
 
+    waitUserInput(_)
+    cv2.destroyAllWindows()
+    track()
+    cv2.destroyAllWindows()
+    #wrapping into video
+    video = cv2.VideoWriter(
+        filename=(output_name + ".mp4"), fourcc=cv2.VideoWriter_fourcc(*"mp4v"), fps=frameRate,
+        frameSize=size
+    )
+    for i in range(len(v)):
+        video.write(v[i])
+    video.release()
+    return True
 
-waitUserInput(_)
-track()
-video = cv2.VideoWriter(
-    filename="result1.mp4", fourcc=cv2.VideoWriter_fourcc(*"mp4v"), fps=frameRate, frameSize=size
-)
-for i in range(len(v)):
-    video.write(v[i])
-video.release()
+#track_video("projectoid2.mp4", "projectoid2", True)
